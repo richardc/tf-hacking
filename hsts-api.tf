716f5af96b543b0288431fdf70b803d0ba44b5f6
Richard Clamp <richardc@unixbeard.net>
2016-08-23 22:16:58 +0100

    Don't need a resource
    We can use the rest_api's root_resource_id as we want to set up /

--- hsts-api.tf
+++ hsts-api.tf
@@ -3,36 +3,30 @@ resource "aws_api_gateway_rest_api" "HSTS" {
   description = "HSTS response app" 
 }
 
-resource "aws_api_gateway_resource" "HSTS" {
-  rest_api_id = "${aws_api_gateway_rest_api.HSTS.id}"
-  parent_id = "${aws_api_gateway_rest_api.HSTS.root_resource_id}"
-  path_part = "/"
-}
-
 resource "aws_api_gateway_method" "HSTS" {
   rest_api_id = "${aws_api_gateway_rest_api.HSTS.id}"
-  resource_id = "${aws_api_gateway_resource.HSTS.id}"
+  resource_id = "${aws_api_gateway_rest_api.HSTS.root_resource_id}"
   http_method = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "HSTS" {
   rest_api_id = "${aws_api_gateway_rest_api.HSTS.id}"
-  resource_id = "${aws_api_gateway_resource.HSTS.id}"
+  resource_id = "${aws_api_gateway_rest_api.HSTS.root_resource_id}"
   http_method = "${aws_api_gateway_method.HSTS.http_method}"
   type = "MOCK"
 }
 
 resource "aws_api_gateway_method_response" "301" {
   rest_api_id = "${aws_api_gateway_rest_api.HSTS.id}"
-  resource_id = "${aws_api_gateway_resource.HSTS.id}"
+  resource_id = "${aws_api_gateway_rest_api.HSTS.root_resource_id}"
   http_method = "${aws_api_gateway_method.HSTS.http_method}"
   status_code = "301"
 }
 
 resource "aws_api_gateway_integration_response" "HSTS" {
   rest_api_id = "${aws_api_gateway_rest_api.HSTS.id}"
-  resource_id = "${aws_api_gateway_resource.HSTS.id}"
+  resource_id = "${aws_api_gateway_rest_api.HSTS.root_resource_id}"
   http_method = "${aws_api_gateway_method.HSTS.http_method}"
   status_code = "${aws_api_gateway_method_response.301.status_code}"
 }
